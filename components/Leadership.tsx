7a34943ce1c57e006b36254b4032617e24790517
ryan <ryanmaglaqui84@gmail.com>
2026-07-20 09:45:05 +0800

    leader ship layout

--- components/Leadership.tsx
+++ components/Leadership.tsx
@@ -51,7 +51,7 @@ export default function Leadership() {
           </p>
         </motion.div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {leaders.map((leader, index) => {
             const LeaderAvatar = ({ leader }: { leader: Leader }) => {
               const [imageError, setImageError] = useState(false);
