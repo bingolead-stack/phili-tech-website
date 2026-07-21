bb2663a62782e276c4670a478a4181bcaa08deed
ryan <ryanmaglaqui84@gmail.com>
2026-07-21 15:54:47 +0800

    partners image size control

--- components/Partner.tsx
+++ components/Partner.tsx
@@ -71,7 +71,7 @@ export default function Partner() {
                     >
                     <div className="absolute inset-0 bg-gradient-to-br from-brand-cyan/5 to-transparent rounded-xl opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
                     <div className="relative z-10">
-                        <div className="inline-flex items-center justify-center">
+                        <div className="inline-flex items-center justify-center image-container">
                         <>
                                 <img
                                     src={partner.image}
