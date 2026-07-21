8c3d09789d6d9ca80cb0f3fdce5233444d786263
ryan <ryanmaglaqui84@gmail.com>
2026-07-21 15:38:42 +0800

    fixed layout

--- components/Partner.tsx
+++ components/Partner.tsx
@@ -57,7 +57,7 @@ export default function Partner() {
             <ArrowRight size={20} className="group-hover:translate-x-1 transition-transform" />
           </Link> */}
         </motion.div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {partners.map((partner, index) => {
                 
             return (
